d440e52e670e66e8cddf433aa8a2a7c83a5c582c
Theo Paris <theoparisdesigns@gmail.com>
2022-06-01 12:53:10 -0700

    fix: performance improvements

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::path::Path;
+use std::time::Instant;
 
 //use adw::{Application, ApplicationWindow};
 //use gtk4::gdk_pixbuf::Pixbuf;
@@ -54,78 +55,82 @@ fn main() {
 	)
 	.expect("Failed to parse config");
 
-	let render_data: Vec<u8> = (0..image_width)
-		.into_par_iter()
-		.map(|x| {
-			(0..image_height)
-				.into_par_iter()
-				.map(|y| {
-					let uv = Vector2::new(
-						x as f32 - 0.5 * image_width as f32,
-						y as f32 - 0.5 * image_height as f32,
-					);
-					let ray_dir = Vector3::new(
-						uv.x as f32,
-						uv.y as f32,
-						image_height as f32,
-					)
+	let start = Instant::now();
+
+	let mut render_data = vec![0; (image_width * image_height) as usize * 4];
+
+	render_data
+		.par_chunks_mut(4)
+		.enumerate()
+		.for_each(|(i, pixel)| {
+			let x = i as i32 % image_width;
+			let y = i as i32 / image_width;
+			let uv = Vector2::new(
+				x as f32 - 0.5 * image_width as f32,
+				y as f32 - 0.5 * image_height as f32,
+			);
+			let ray_dir =
+				Vector3::new(uv.x as f32, uv.y as f32, image_height as f32)
 					.normalize();
 
-					let mut scene = Scene::default();
-					scene.add(Box::new(Sphere {
-						center: Vector3::new(0.0, 0.0, 0.0),
-						radius: 0.5,
-					}));
-					//scene.add(Box::new(Cube {
-					//center: Vector3::new(0.0, 3.0, 0.0),
-					//size: Vector3::new(0.6, 0.6, 0.5),
-					//}));
+			let mut scene = Scene::default();
+			scene.add(Box::new(Sphere {
+				center: Vector3::new(0.0, 0.0, 0.0),
+				radius: 0.5,
+			}));
+			//scene.add(Box::new(Cube {
+			//center: Vector3::new(0.0, 3.0, 0.0),
+			//size: Vector3::new(0.6, 0.6, 0.5),
+			//}));
 
-					let scene = Box::new(scene);
+			let scene = Box::new(scene);
 
-					let march_settings = MarchSettings {
-						origin: cam_pos,
-						direction: ray_dir,
-						max_dist: 1000.0,
-						min_dist: 0.0001,
-						steps: config.steps,
-						shape: scene,
-						light_position: Vector3::new(2.0, 2.0, 7.0),
-					};
-					let ray = Ray::march(march_settings);
+			let march_settings = MarchSettings {
+				origin: cam_pos,
+				direction: ray_dir,
+				max_dist: 1000.0,
+				min_dist: 0.0001,
+				steps: config.steps,
+				shape: scene,
+				light_position: Vector3::new(2.0, 2.0, 7.0),
+			};
+			let ray = Ray::march(march_settings);
 
-					if ray.hit {
-						let color = Vector3::new(
-							ray.diffuse_reflection,
-							ray.diffuse_reflection,
-							ray.diffuse_reflection,
-						)
-						.component_mul(&config.object_color)
-							* 255.0;
+			if ray.hit {
+				let color = Vector3::new(
+					ray.diffuse_reflection,
+					ray.diffuse_reflection,
+					ray.diffuse_reflection,
+				)
+				.component_mul(&config.object_color)
+					* 255.0;
 
-						vec![color.x as u8, color.y as u8, color.z as u8, 255]
-					} else {
-						let glow = f32::max(
-							config.glow_amount - ray.glow_distance * 20.0,
-							0.0,
-						);
-						let color = Vector3::new(glow, glow, glow)
-							.component_mul(&config.glow_color)
-							* 255.0;
+				pixel.copy_from_slice(&[
+					color.x as u8,
+					color.y as u8,
+					color.z as u8,
+					255,
+				]);
+			} else {
+				let glow = f32::max(
+					config.glow_amount - ray.glow_distance * 20.0,
+					0.0,
+				);
+				let color = Vector3::new(glow, glow, glow)
+					.component_mul(&config.glow_color)
+					* 255.0;
 
-						vec![
-							(color.x) as u8,
-							(color.y) as u8,
-							(color.z) as u8,
-							255,
-						]
-					}
-				})
-				.flatten()
-				.collect::<Vec<u8>>()
-		})
-		.flatten()
-		.collect();
+				pixel.copy_from_slice(&[
+					color.x as u8,
+					color.y as u8,
+					color.z as u8,
+					255,
+				]);
+			}
+		});
+
+	let duration = start.elapsed();
+	println!("Time elapsed: {:?}", duration);
 
 	image::save_buffer(
 		&Path::new("image.png"),
